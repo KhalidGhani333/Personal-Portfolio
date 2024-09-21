c40bac2761beb01625c602b95b8b9fb998cc9421
KhalidGhani333 <khalidghani.333@gmail.com>
2024-09-21 23:54:05 +0500

    Push code to github

---
+++ src/app/components/projects/page.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div>
+    <section className='mt-60'>
+      <h1 className='text-white text-4xl text-center mt-10 animate-bounce duration-500'><span>My</span> Pro<span>je</span>cts</h1>
+      <div className='text-white justify-items-center mt-40 grid grid-cols-2 md:grid-cols-3 text-center'>
+      
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/countdown.png" alt="Countdown" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500 "/>
+        <a href="https://count-down-timer-lovat.vercel.app/" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Count Down Timer</h3></a>
+       </div>
+
+       <div className=" items-center justify-center p-4">
+      <img 
+        src="/resumebuilder.webp" alt="resumebuilder"  className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500 "/>
+        <a href="https://hackathon-milestone-3-4-5.vercel.app/" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Interactive Resume Builder</h3></a>
+       </div>
+
+       <div className=" items-center justify-center p-4">
+      <img
+        src="/timebox.jpg" alt="Ecommerce web" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/WebPage.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Ecommerce Web</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/consoleInteractiveTable" alt="interactive tabes" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/Console-Interactive-Table.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Console Interactive Tables</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/digitalclock.png" alt="digital clock" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/Digital-Clock.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Digital Clock</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/bankAccount.png" alt="bank account system" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/OOP-Mybank.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Bank Account system</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/quiz" alt="quiz system" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/Quiz-System.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Quiz System</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/adventureGame.jpg" alt="adventure game" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/Adventure-Game.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Adventure Game</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/student-management.webp" alt="student management system" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/Student-Magement-System.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Student Management System</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/currency_converter.jpg" alt="currency convertor" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/Currency-Convertor.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Currency Convertor</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/To-Do_List_App.webp" alt="todo app" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/To-Do-List-.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>To-Do List App</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/Atm-Machine.jpg" alt="atm machine" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/ATM-Machine.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>ATM Machine</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/number-gussing.jpg" alt="number guessing game" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/Number-guessing-game.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Number Guessing Game</h3></a>
+      </div>
+
+      <div className=" items-center justify-center p-4">
+      <img
+        src="/calculator.jpg" alt="calculator" className="w-64 h-40 rounded-lg shadow-lg shadow-blue-300 hover:scale-105 duration-500"/>
+        <a href="https://github.com/KhalidGhani333/Calculator.git" target="_blank"><h3 className='mt-6 mb-10 underline hover:text-blue-500'>Calculator</h3></a>
+      </div>
+      
+      </div>
+      
+        </section>
+    </div>
+  )
+}
+
+export default Project
